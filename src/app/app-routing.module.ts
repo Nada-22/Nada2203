360ebfbcf4c86a94bc1199b196a8313a8564c67f
Nada Hassan <nada.h22425@gmail.com>
2025-03-21 04:58:19 +0200

    setup layout module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LayoutComponent } from './layout/layout.component';
+import { LayoutModule } from './layout/layout.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: LayoutComponent, children: [
+      // { path: '', redirectTo: 'home', pathMatch: 'full' },
+      // { path: 'home', component: HomeComponent },
+    ]
+  }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes) , LayoutModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { LayoutModule } from "./layout/layout.module";
 
 @NgModule({
   declarations: [
@@ -10,8 +12,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
-  ],
+    AppRoutingModule,
+    LayoutModule
+],
   providers: [],
   bootstrap: [AppComponent]
 })
